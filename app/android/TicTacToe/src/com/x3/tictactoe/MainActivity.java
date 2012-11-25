fd297d620ba47cf32093c3bc45713aa1bbcdb621
manuna <awp.manuna@gmail.com>
2012-11-26 00:17:58 +0200

    Updated UI. Added event handlers. Added mockup in png format

--- app/android/TicTacToe/src/com/x3/tictactoe/MainActivity.java
+++ app/android/TicTacToe/src/com/x3/tictactoe/MainActivity.java
@@ -2,7 +2,9 @@ package com.x3.tictactoe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +18,17 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void on3x3Clicked(View view) {
+    	Log.v("MainActivity", "3x3 button selected");
+    }
+    
+    public void on5x5Clicked(View view) {
+    	Log.v("MainActivity", "5x5 button selected");
+    }
+    
+    public void onRestartGame(View view) {
+    	Log.v("MainActivity", "Restarting game");
     }
 }
