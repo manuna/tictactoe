a9ae5cd448bff283f556d605f820e35e48b473d2
manuna <awp.manuna@gmail.com>
2012-11-27 09:31:25 +0200

    Basic implementation of playground. It is needed to correct layout

--- app/android/TicTacToe/src/com/x3/tictactoe/MainActivity.java
+++ app/android/TicTacToe/src/com/x3/tictactoe/MainActivity.java
@@ -5,10 +5,15 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends Activity {
 	
 	private View mSelectedSegment = null;
+	private TableLayout mPlayground3x3 = null;
+	private TableLayout mPlayground5x5 = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,12 @@ public class MainActivity extends Activity {
         
         // Initialize UI
         selectSegment(findViewById(R.id.button_3x3));
+        
+        mPlayground3x3 = (TableLayout)findViewById(R.id.playground_root_3x3);
+        generatePlayground(mPlayground3x3, 3);
+        
+        mPlayground5x5 = (TableLayout)findViewById(R.id.playground_root_5x5);
+        generatePlayground(mPlayground5x5, 5);
     }
 
     @Override
@@ -27,11 +38,23 @@ public class MainActivity extends Activity {
     
     public void on3x3Clicked(View view) {
     	Log.v("MainActivity", "3x3 button selected");
+    	
+    	if (view != mSelectedSegment) {
+    		mPlayground5x5.setVisibility(View.INVISIBLE);
+    		mPlayground3x3.setVisibility(View.VISIBLE);
+    	}
+    	
     	selectSegment(view);
     }
     
     public void on5x5Clicked(View view) {
     	Log.v("MainActivity", "5x5 button selected");
+    	
+    	if (view != mSelectedSegment) {
+    		mPlayground5x5.setVisibility(View.VISIBLE);
+    		mPlayground3x3.setVisibility(View.INVISIBLE);
+    	}
+    	
     	selectSegment(view);
     }
     
@@ -46,6 +69,23 @@ public class MainActivity extends Activity {
     		}
     		mSelectedSegment = view;
     		mSelectedSegment.setSelected(true);
+    	}
+    }
+    
+    private void generatePlayground(TableLayout layout, int size) {
+    	layout.removeAllViews();
+    	    	
+    	for (int i = 0; i < size; i++) {
+    		TableRow row = new TableRow(this);
+    		for (int j = 0; j < size; j++) {
+    			ImageButton cell = new ImageButton(this);
+    			cell.setBackgroundResource(R.drawable.game_cell);
+    			row.addView(cell,  new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT));
+    		}
+    		    		
+    		layout.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT));
     	}
     }
 }
