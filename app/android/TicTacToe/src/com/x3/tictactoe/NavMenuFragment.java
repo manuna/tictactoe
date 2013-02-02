728408407f8e3bdc6514becd2e02bb38fbda4d0f
manuna <awp.manuna@gmail.com>
2013-02-02 21:44:40 +0200

    Applied new design to the main menu

--- app/android/TicTacToe/src/com/x3/tictactoe/NavMenuFragment.java
+++ app/android/TicTacToe/src/com/x3/tictactoe/NavMenuFragment.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class NavMenuFragment extends Fragment {
-
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
